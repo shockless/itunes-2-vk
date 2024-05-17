5ac211fdf6416a0041e7c89dc8b688afc611add2
Федор Авилов <fedoravilov@MacBook-Pro-Fedor.local>
2024-05-18 01:57:33 +0300

    yandex music csv importer

---
+++ yandex_importer/importer.py
@@ -0,0 +1,74 @@
+import click
+import pandas as pd
+from tqdm.auto import tqdm
+from yandex_music import Client
+
+
+def get_id(result, stype):
+    if result[stype] is None:
+        if stype in result['best']['result']:
+            id_obj = result['best']['result'][stype]
+        else:
+            return None
+    else:
+        id_obj = result[stype]['results']
+
+    if len(id_obj) > 0 and 'id' in id_obj[0]:
+        return id_obj[0]['id']
+    else:
+        if 'id' not in id_obj[0]:
+            print(id_obj[0], stype)
+        return None
+
+
+@click.command()
+@click.option('--api_key', prompt='API Key',
+              help='Yandex Music API Key')
+@click.option('--library', prompt='CSV Path',
+              help='Path to library csv')
+def main(api_key, library):
+    client = Client(api_key).init()
+    library_df = pd.read_csv(library)
+    type2dict = {'Album': 'albums', 'Artist': 'artists', 'Favorite': 'tracks', 'Playlist': 'playlists'}
+
+    user_playlists = client.users_playlists_list()
+    playlists = {playlist.title: playlist.kind for playlist in user_playlists}
+
+    for ind, row in tqdm(library_df.iterrows(), total=len(library_df)):
+        name, artist, album, playlist, stype, _ = row
+        if pd.isna(artist) and stype != 'Artist':
+            continue
+
+        elif stype == 'Artist':
+            search_query = ' - '.join([name])
+        else:
+            search_query = ' - '.join([artist, name])
+
+        result = client.search(search_query).to_dict()
+        if result['best'] is None:
+            print(search_query, 'not found')
+            continue
+
+        stype = type2dict[stype]
+        if stype == 'playlists':
+            result_id = get_id(result, 'tracks')
+        else:
+            result_id = get_id(result, stype)
+
+        if result_id is None:
+            print(search_query, 'not found')
+            continue
+
+        if stype == 'playlists':
+            if playlist not in playlists:
+                playlist_obj = client.users_playlists_create(playlist)
+                playlists[playlist] = playlist_obj.kind
+            album_id = client.tracks(result_id)[0]['albums'][0]['id']
+            print("Not Supported")
+            # client.users_playlists_insert_track(playlists[playlist], result_id, album_id, revision=2)
+        else:
+            eval(f'client.users_likes_{stype}_add')(result_id)
+
+
+if __name__ == '__main__':
+    main()
